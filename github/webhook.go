b7f386b69ca57b46bdb47cc9aa713a21667321b4
Sammy Teillet <sammy.teillet@gmail.com>
2023-04-28 20:26:27 +0200

    feature: validate datadrift-config.json

--- github/webhook.go
+++ github/webhook.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-github/github"
+	"github.com/xeipuuv/gojsonschema"
 )
 
 type GithubWebhookPayload struct {
@@ -51,31 +53,56 @@ func HandleWebhook(c *gin.Context) {
 
 	if payload.Repository.Owner.Name != "" {
 
-		// Get the last commit of the repository
-		commit, _, err := client.Repositories.GetCommit(ctx, payload.Repository.Owner.Name, payload.Repository.Name, "main")
+		confidIsValid, err := verifyConfigFile(client, payload.Repository.Owner.Name, payload.Repository.Name, ctx)
 
 		if err != nil {
-			fmt.Println("wahou 2")
 			c.JSON(http.StatusBadRequest, gin.H{"error": err})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"message": "Webhook processed", "commit": commit.GetSHA(), "installationId": InstallationId})
+		c.JSON(http.StatusOK, gin.H{"message": "Webhook processed", "configIsValie": confidIsValid, "installationId": InstallationId})
 	} else if payload.Installation.Account.Login != "" {
-		// Get the last commit of the repository
-		commit, _, err := client.Repositories.GetCommit(ctx, payload.Installation.Account.Login, payload.Repositories[0].Name, "main")
-
+		confidIsValid, err := verifyConfigFile(client, payload.Installation.Account.Login, payload.Repositories[0].Name, ctx)
 		if err != nil {
-			fmt.Println("wahou 3")
 			c.JSON(http.StatusBadRequest, gin.H{"error": err})
 			return
 		}
-
-		c.JSON(http.StatusOK, gin.H{"message": "Webhook processed", "commit": commit.GetSHA(), "installationId": InstallationId})
+		c.JSON(http.StatusOK, gin.H{"message": "Webhook processed", "configIsValie": confidIsValid, "installationId": InstallationId})
 	} else {
 		fmt.Println("No repository or account found")
 
 		c.JSON(http.StatusBadRequest, gin.H{"error": "No repository or account found"})
 		return
 	}
+}
+
+func verifyConfigFile(client *github.Client, RepoOwner string, RepoName string, ctx context.Context) (bool, error) {
+
+	commit, _, _ := client.Repositories.GetCommit(ctx, RepoOwner, RepoName, "main")
+
+	configFilePath := "datadrift-config.json"
+
+	file, _, _, err := client.Repositories.GetContents(ctx, RepoOwner, RepoName, configFilePath, &github.RepositoryContentGetOptions{
+		Ref: *commit.SHA,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+	content, _ := file.GetContent()
+	schemaLoader := gojsonschema.NewReferenceLoader("file://./json-schema.json")
+	documentLoader := gojsonschema.NewStringLoader(content)
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+	if result.Errors() != nil {
+		fmt.Println("result.Errors()", result.Errors())
+		return false, fmt.Errorf("invalid config file")
+	}
+	fmt.Println(result.Valid())
+	return true, nil
 }
