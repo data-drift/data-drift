26d2920c9a04ed7057d06354ce57cf46c52d4685
Sammy Teillet <sammy.teillet@gmail.com>
2023-04-28 17:04:39 +0200

    refactor: extract client factory

---
+++ github/client-factory.go
@@ -0,0 +1,66 @@
+package github
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+func CreateClientFromGithubApp() (*github.Client, error) {
+	privateKeyPath := os.Getenv("GITHUB_APP_PRIVATE_KEY_PATH")
+	privateKey := os.Getenv("GITHUB_APP_PRIVATE_KEY")
+
+	appIDStr := os.Getenv("GITHUB_APP_ID")
+	githubAppId, err := strconv.ParseInt(appIDStr, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	itr, err := CreateGithubTransport(privateKeyPath, privateKey, githubAppId, 36944435)
+	if err != nil {
+		return nil, err
+	}
+
+	// Use installation transport with client.
+	client := github.NewClient(&http.Client{Transport: itr})
+	return client, nil
+}
+
+func CreateClientFromGithubToken(token string) *github.Client {
+	client := github.NewClient(nil)
+	if strings.HasPrefix(token, "github_pat") {
+		// Create a new GitHub client with authentication using the token.
+		ctx := context.Background()
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: token},
+		)
+		tc := oauth2.NewClient(ctx, ts)
+		client = github.NewClient(tc)
+	}
+	return client
+}
+
+func CreateGithubTransport(privateKeyPath string, privateKey string, githubAppId int64, githubInstallationId int64) (*ghinstallation.Transport, error) {
+	if privateKeyPath != "" {
+		itr, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, githubAppId, githubInstallationId, privateKeyPath)
+		if err != nil {
+			return nil, err
+		}
+		return itr, nil
+	} else if privateKey != "" {
+		itr, err := ghinstallation.New(http.DefaultTransport, githubAppId, githubInstallationId, []byte(privateKey))
+		if err != nil {
+			return nil, err
+		}
+		return itr, nil
+	} else {
+		return nil, fmt.Errorf("missing GitHub App private key information")
+	}
+}
--- github/github.go
+++ github/github.go
@@ -2,32 +2,14 @@ package github
 
 import (
 	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"strconv"
-
-	"github.com/bradleyfalzon/ghinstallation/v2"
-	"github.com/google/go-github/github"
 )
 
 func CheckGithubAppConnection() (string, error) {
-	privateKeyPath := os.Getenv("GITHUB_APP_PRIVATE_KEY_PATH")
-	privateKey := os.Getenv("GITHUB_APP_PRIVATE_KEY")
 
-	appIDStr := os.Getenv("GITHUB_APP_ID")
-	githubAppId, err := strconv.ParseInt(appIDStr, 10, 64)
+	client, err := CreateClientFromGithubApp()
 	if err != nil {
 		return "", err
 	}
-
-	itr, err := CreateGithubTransport(privateKeyPath, privateKey, githubAppId, 36944435)
-	if err != nil {
-		return "", err
-	}
-
-	// Use installation transport with client.
-	client := github.NewClient(&http.Client{Transport: itr})
 	ctx := context.Background()
 
 	// Get the last commit of the repository
@@ -37,22 +19,4 @@ func CheckGithubAppConnection() (string, error) {
 		return "", err
 	}
 	return commit.GetSHA(), nil
-}
-
-func CreateGithubTransport(privateKeyPath string, privateKey string, githubAppId int64, githubInstallationId int64) (*ghinstallation.Transport, error) {
-	if privateKeyPath != "" {
-		itr, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, githubAppId, githubInstallationId, privateKeyPath)
-		if err != nil {
-			return nil, err
-		}
-		return itr, nil
-	} else if privateKey != "" {
-		itr, err := ghinstallation.New(http.DefaultTransport, githubAppId, githubInstallationId, []byte(privateKey))
-		if err != nil {
-			return nil, err
-		}
-		return itr, nil
-	} else {
-		return nil, fmt.Errorf("missing GitHub App private key information")
-	}
 }
