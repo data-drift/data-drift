83f588f12819263abb545144ee40aeb9edc5f55c
Sammy Teillet <sammy.teillet@gmail.com>
2023-05-01 13:43:19 +0200

    feature: add debug mode

---
+++ debug/debug.go
@@ -0,0 +1,38 @@
+package debug
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/data-drift/kpi-git-history/charts"
+	"github.com/data-drift/kpi-git-history/common"
+	"github.com/data-drift/kpi-git-history/github"
+	"github.com/data-drift/kpi-git-history/history"
+	"github.com/data-drift/kpi-git-history/reports"
+)
+
+func DebugFunction() {
+	// Perform debugging operations
+	fmt.Println("Manual Sync ...")
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	githubRepoOwner := os.Getenv("GITHUB_REPO_OWNER")
+	githubRepoName := os.Getenv("GITHUB_REPO_NAME")
+	githubRepoFilePath := os.Getenv("GITHUB_REPO_FILE_PATH")
+	dateColumn := os.Getenv("DATE_COLUMN")
+	startDate := os.Getenv("START_DATE")
+	kpiColumn := os.Getenv("KPI_COLUMN")
+	notionAPIKey := os.Getenv("NOTION_API_KEY")
+	notionDatabaseID := os.Getenv("NOTION_DATABASE_ID")
+
+	client := github.CreateClientFromGithubToken(githubToken)
+	filepath, err := history.ProcessHistory(client, githubRepoOwner, githubRepoName, githubRepoFilePath, startDate, dateColumn, kpiColumn)
+	chartResults := charts.ProcessCharts(filepath)
+
+	for _, chartResult := range chartResults {
+		err = reports.CreateReport(common.SyncConfig{NotionAPIKey: notionAPIKey, NotionDatabaseID: notionDatabaseID}, chartResult)
+		if err != nil {
+			println(err)
+		}
+	}
+	println(filepath, err)
+}
