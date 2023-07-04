1f5aae099b41fd5a91b251fd8ac09e49a17d8b29
Sammy Teillet <sammy.teillet@gmail.com>
2023-07-04 14:43:14 +0200

    chore(datagit): deploy new readme

--- tools/datagit/setup.py
+++ tools/datagit/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datagit",
-    version="0.8",
+    version="0.9",
     packages=find_packages(),
     author="Sammy Teillet",
     author_email="sammy.teillet@gmail.com",
