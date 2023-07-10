fb3cc5f10f785c4959d2d1e562e61bd4c8087810
Sammy Teillet <sammy.teillet@gmail.com>
2023-07-10 10:25:16 +0200

    docs(datagit): add documentation for the store metric metthod

--- tools/datagit/datagit/github_connector.py
+++ tools/datagit/datagit/github_connector.py
@@ -17,6 +17,21 @@ def store_metric(
     branch: Optional[str] = None,
     store_json: bool = True,
 ) -> None:
+    """
+    Store metrics into a specific repository file on Github.
+    Parameters:
+    ghClient (Github): An instance of a Github client to interact with the Github API.
+    dataframe (pd.DataFrame): The dataframe containing the metrics to be stored.
+    filepath (str): The full path to the target file in the format 'organization/repository/path_to_file'.
+    assignees (List[str]): List of Github usernames to be assigned to the pull request. Defaults to an empty list. If list is empty no alert will be raised, nor pull request will be created.
+    branch (Optional[str]): The name of the branch where the metrics will be stored. If None, a branch name will be generated f"metric/{filepath}". Defaults to None.
+    store_json (bool): If True, stores the dataframe in the .json format. Defaults to True.
+    Returns:
+    None: This function does not return any value, but it performs a side effect of pushing the metric to Github.
+    Raises:
+    ValueError: If the dataframe does not have a unique first column or if the file path does not have the format 'organization/repository/path_to_file'.
+    GithubException: If there is an error in interacting with the Github API, e.g., insufficient permissions, non-existent repo, etc.
+    """
     print("Storing metric...")
     repo_orga, repo_name, file_path = filepath.split("/", 2)
     branch = branch or get_valid_branch_name(file_path)
