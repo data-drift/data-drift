fcddbe77ab937b1e3aa026dff6f8aaff8cfa923f
Sammy Teillet <sammy.teillet@gmail.com>
2023-07-17 18:21:27 +0200

    bugfix: use brackets instead of dot notation... not sure why but it works

--- tools/datagit/datagit/drift_evaluators.py
+++ tools/datagit/datagit/drift_evaluators.py
@@ -3,8 +3,8 @@ from datagit.dataset_helpers import compare_dataframes
 
 def default_drift_evaluator(data_drift_context):
     alert_message = f"Drift detected:\n" + compare_dataframes(
-        data_drift_context.reported_dataframe,
-        data_drift_context.computed_dataframe,
+        data_drift_context["reported_dataframe"],
+        data_drift_context["computed_dataframe"],
         "unique_key",
     )
     return {"should_alert": True, "message": alert_message}
