5c28d97f89362797ce7e8a426bdb66330de6f030
Sammy Teillet <sammy.teillet@gmail.com>
2023-07-18 15:24:34 +0200

    chore: bump version

--- tools/datagit/setup.py
+++ tools/datagit/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datagit",
-    version="0.13",
+    version="0.14",
     packages=find_packages(),
     author="Sammy Teillet",
     author_email="sammy.teillet@gmail.com",
