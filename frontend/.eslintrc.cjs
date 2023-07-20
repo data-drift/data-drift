d9f2b494d5fc876721feeab17f6baba7f010e628
Sammy Teillet <sammy.teillet@gmail.com>
2023-07-20 15:42:19 +0200

    chore: install storybook

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,26 +2,23 @@
 
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
-  ],
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: true,
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-}
+    'react-refresh/only-export-components': ['warn', {
+      allowConstantExport: true
+    }],
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  }
+};
