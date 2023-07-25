4cf0ff118d58610e1006af5daae3ca03bb317838
Sammy Teillet <sammy.teillet@gmail.com>
2023-07-25 17:30:37 +0200

    feature: use token from local storage

--- frontend/src/services/github.ts
+++ frontend/src/services/github.ts
@@ -4,13 +4,26 @@ import { Endpoints } from "@octokit/types";
 type CommitResponse =
   Endpoints["GET /repos/{owner}/{repo}/commits/{ref}"]["response"];
 
+const getRequestHeaders = () => {
+  const githubAccessToken = localStorage.getItem("github_token");
+  if (githubAccessToken) {
+    return {
+      Authorization: `bearer ${githubAccessToken}`,
+    };
+  } else {
+    return {};
+  }
+};
+
 export const getCommitFiles = async (
   owner: string,
   repo: string,
   commitSHA: string
 ) => {
+  const headers = getRequestHeaders();
   const response = await axios.get<CommitResponse["data"]>(
-    `https://api.github.com/repos/${owner}/${repo}/commits/${commitSHA}`
+    `https://api.github.com/repos/${owner}/${repo}/commits/${commitSHA}`,
+    { headers }
   );
   if (response.status !== 200) {
     throw new Error("Error fetching commit content");
