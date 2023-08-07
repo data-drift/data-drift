2fe4af088094786c5777ed664c605f721a2441ed
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-07 12:29:47 +0200

    feature: return patch of commit

--- backend/github/get-commit-diff.go
+++ backend/github/get-commit-diff.go
@@ -1,12 +1,45 @@
 package github
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetCommitDiff(c *gin.Context) {
-	installationId := c.Request.Header.Get("Installation-Id")
+	InstallationId, err := strconv.ParseInt(c.Request.Header.Get("Installation-Id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "error parsing Installation-Id header"})
+		return
+	}
 	owner := c.Param("owner")
 	repo := c.Param("repo")
 	commitSha := c.Param("commit-sha")
 
-	print(installationId, owner, repo, commitSha)
+	client, err := CreateClientFromGithubApp(int64(InstallationId))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	commit, _, ghErr := client.Repositories.GetCommit(c, owner, repo, commitSha, nil)
+	if ghErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": ghErr})
+		return
+	}
+	if len(commit.Files) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "no files in commit"})
+		return
+	}
+
+	firstPatch := commit.Files[0].GetPatch()
+	jsonData, err := json.Marshal(gin.H{"patch": firstPatch})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error marshaling JSON"})
+		return
+	}
+
+	c.Data(http.StatusOK, "application/json", jsonData)
 }
