510d1d898298603b7deb0aa5929963391f4a881b
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-07 11:11:40 +0200

    feature: reimplement the 2 routes with router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import GithubForm from "./pages/GithubForm";
+import DisplayCommit from "./pages/DisplayCommit";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <GithubForm />,
+  },
+  {
+    path: "/:owner/:repo/commit/:commitSHA",
+    element: <DisplayCommit />,
   },
 ]);
