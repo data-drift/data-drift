8d25902416ca288aa4f0915f39f5ed8a5f44c288
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-08 18:44:24 +0200

    test: add usecase with a lot of data

--- frontend/src/components/Charts/StepChart.stories.tsx
+++ frontend/src/components/Charts/StepChart.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { StepChart } from "./StepChart";
+import { StepChart, YearMonthString } from "./StepChart";
+
+import { payload } from "./payload";
 
 const meta = {
   title: "Charts/StepChart",
@@ -48,57 +50,41 @@ export const SingleMetric: Story = {
   },
 };
 
-export const MultipleMetrics: Story = {
-  args: {
-    metricNames: ["2023-02", "2022-01"],
+const mapPayloadToArgs = (data: typeof payload): Story["args"] => {
+  console.log(data);
+  const result = data.datasets.reduce<Story["args"]>(
+    (
+      acc: {
+        metricNames: Story["args"]["metricNames"];
+        data: Story["args"]["data"];
+      },
+      dataset
+    ) => {
+      const newData = acc.data;
+      const metricName = dataset.label as YearMonthString;
+      dataset.data.forEach((dataPoint) => {
+        if (newData[dataPoint.x]) {
+          newData[dataPoint.x][metricName] = dataPoint.y;
+        } else {
+          newData.push({
+            daysSinceFirstReport: dataPoint.x,
+            [dataset.label]: dataPoint.y,
+          });
+        }
+      });
+      return {
+        metricNames: [...acc.metricNames, metricName].sort(),
+        data: newData,
+      };
+    },
+    {
+      metricNames: [] as Story["args"]["metricNames"],
+      data: [] as Story["args"]["data"],
+    }
+  );
+  return result;
+};
 
-    data: [
-      { daysSinceFirstReport: 0.4480324074074074, "2023-02": 0 },
-      {
-        daysSinceFirstReport: 15.706261574074075,
-        "2023-02": -0.17640146440354,
-      },
-      {
-        daysSinceFirstReport: 16.486157407407408,
-        "2023-02": -0.11398248469152,
-      },
-      {
-        daysSinceFirstReport: 17.230462962962964,
-        "2023-02": -0.17640146440354,
-      },
-      {
-        daysSinceFirstReport: 20.222453703703703,
-        "2023-02": -0.11398248469152,
-      },
-      {
-        daysSinceFirstReport: 49.37809027777778,
-        "2023-02": 0.31480876724324,
-      },
-      {
-        daysSinceFirstReport: 57.243946759259266,
-        "2023-02": 0.44869295561106,
-      },
-      { daysSinceFirstReport: 344.423287037037, "2022-01": 0 },
-      { daysSinceFirstReport: 344.4266087962963, "2022-01": 0 },
-      { daysSinceFirstReport: 344.4275, "2022-01": 0 },
-      { daysSinceFirstReport: 344.42751157407406, "2022-01": 0 },
-      { daysSinceFirstReport: 344.4289814814815, "2022-01": 0 },
-      { daysSinceFirstReport: 344.42899305555557, "2022-01": 0 },
-      { daysSinceFirstReport: 344.48516203703707, "2022-01": 0 },
-      { daysSinceFirstReport: 345.22763888888886, "2022-01": 0 },
-      { daysSinceFirstReport: 346.579525462963, "2022-01": 0 },
-      { daysSinceFirstReport: 350.2256597222222, "2022-01": 0 },
-      { daysSinceFirstReport: 357.4198263888889, "2022-01": 0 },
-      { daysSinceFirstReport: 364.2250925925926, "2022-01": 0 },
-      { daysSinceFirstReport: 365.47398148148153, "2022-01": 0 },
-      { daysSinceFirstReport: 365.47414351851853, "2022-01": 0 },
-      { daysSinceFirstReport: 371.22749999999996, "2022-01": 0 },
-      { daysSinceFirstReport: 372.23555555555555, "2022-01": 0 },
-      { daysSinceFirstReport: 373.6502083333333, "2022-01": 0 },
-      { daysSinceFirstReport: 373.7151157407407, "2022-01": 0 },
-      { daysSinceFirstReport: 379.44136574074076, "2022-01": 0.03263395504281 },
-      { daysSinceFirstReport: 409.48615740740746, "2022-01": 0.45034857959079 },
-      { daysSinceFirstReport: 410.230462962963, "2022-01": 0.03263395504281 },
-    ],
-  },
+export const MultipleMetrics: Story = {
+  args: mapPayloadToArgs(payload),
 };
