d1f166bcfd924481da25577f311844891820b520
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-10 11:05:39 +0200

    chore: extract hardcoded data in props

--- frontend/src/components/Charts/WaterfallChart.stories.tsx
+++ frontend/src/components/Charts/WaterfallChart.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { WaterfallChart } from "./WaterfallChart";
+import { theme } from "../../theme";
 
 const meta = {
   title: "Charts/WaterfallChart",
@@ -14,4 +15,30 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const SimpleCase: Story = {};
+export const SimpleCase: Story = {
+  args: {
+    data: [
+      {
+        day: "05-01",
+        drift: [980, 1000],
+        fill: theme.colors.text,
+        isInitial: true,
+      },
+      {
+        day: "05-02",
+        drift: [1000, 1050],
+        fill: theme.colors.dataUp,
+      },
+      {
+        day: "05-03",
+        drift: [1050, 1020],
+        fill: theme.colors.dataDown,
+      },
+      {
+        day: "05-04",
+        drift: [1020, 1010],
+        fill: theme.colors.dataDown,
+      },
+    ],
+  },
+};
--- frontend/src/components/Charts/WaterfallChart.tsx
+++ frontend/src/components/Charts/WaterfallChart.tsx
@@ -25,31 +25,7 @@ const formatToolTipValueTick: Formatter = (
   return `${tickValue[1]} (${signedDrift})`;
 };
 
-export const WaterfallChart = () => {
-  const data = [
-    {
-      day: "05-01",
-      drift: [980, 1000],
-      fill: theme.colors.text,
-      isInitial: true,
-    },
-    {
-      day: "05-02",
-      drift: [1000, 1050],
-      fill: theme.colors.dataUp,
-    },
-    {
-      day: "05-03",
-      drift: [1050, 1020],
-      fill: theme.colors.dataDown,
-    },
-    {
-      day: "05-04",
-      drift: [1020, 1010],
-      fill: theme.colors.dataDown,
-    },
-  ] satisfies DataItem[];
-
+export const WaterfallChart = ({ data }: { data: DataItem[] }) => {
   return (
     <BarChart
       width={730}
