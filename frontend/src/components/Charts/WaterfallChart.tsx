dc7d04e7544f3474ab735d50b514acfeb0a817b6
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-10 11:03:06 +0200

    feature: display drift in toolbox

--- frontend/src/components/Charts/WaterfallChart.tsx
+++ frontend/src/components/Charts/WaterfallChart.tsx
@@ -1,33 +1,54 @@
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import { ComponentProps } from "react";
+
+// import type { Formatter } from "recharts";
 import { theme } from "../../theme";
 
-const getBarColor = ({ color }: { color: string }) => {
-  return color;
+type TooltipProps = ComponentProps<typeof Tooltip<[number, number], "drift">>;
+type Formatter = TooltipProps["formatter"];
+
+interface DataItem {
+  day: string;
+  drift: [number, number];
+  fill: string;
+  isInitial?: boolean;
+}
+
+const formatToolTipValueTick: Formatter = (
+  tickValue,
+  _name,
+  item: { payload?: DataItem }
+) => {
+  if (item.payload?.isInitial) return `${tickValue[1]}`;
+  const drift = tickValue[1] - tickValue[0];
+  const signedDrift = drift > 0 ? `+${drift}` : drift;
+  return `${tickValue[1]} (${signedDrift})`;
 };
 
 export const WaterfallChart = () => {
   const data = [
     {
       day: "05-01",
-      value: [980, 1000],
+      drift: [980, 1000],
       fill: theme.colors.text,
+      isInitial: true,
     },
     {
       day: "05-02",
-      value: [1000, 1050],
+      drift: [1000, 1050],
       fill: theme.colors.dataUp,
     },
     {
       day: "05-03",
-      value: [1050, 1020],
+      drift: [1050, 1020],
       fill: theme.colors.dataDown,
     },
     {
       day: "05-04",
-      value: [1020, 1010],
+      drift: [1020, 1010],
       fill: theme.colors.dataDown,
     },
-  ];
+  ] satisfies DataItem[];
 
   return (
     <BarChart
@@ -38,8 +59,11 @@ export const WaterfallChart = () => {
     >
       <XAxis dataKey="day" />
       <YAxis domain={[980, "dataMax"]} />
-      <Tooltip />
-      <Bar dataKey="value" />
+      <Tooltip
+        contentStyle={{ backgroundColor: theme.colors.background }}
+        formatter={formatToolTipValueTick}
+      />
+      <Bar dataKey="drift" fill={"white"} />
     </BarChart>
   );
 };
