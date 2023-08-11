fa9d948e524f948ab26c68db56db07b21b34ae58
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-11 10:53:47 +0200

    feature: put url in const

--- frontend/src/services/data-drift.ts
+++ frontend/src/services/data-drift.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { CommitInfo } from "../pages/DisplayCommit";
 
+const DATA_DRIFT_API_URL = "https://data-drift.herokuapp.com";
+
 export const getPatchAndHeader = async (
   params: CommitInfo & { installationId: string }
 ) => {
   const result = await axios.get<{ patch: string; headers: string[] }>(
-    `https://data-drift.herokuapp.com/gh/${params.owner}/${params.repo}/commit/${params.commitSHA}`,
+    `${DATA_DRIFT_API_URL}/gh/${params.owner}/${params.repo}/commit/${params.commitSHA}`,
     { headers: { "Installation-Id": params.installationId } }
   );
   return {
