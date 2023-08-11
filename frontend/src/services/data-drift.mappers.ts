8d279e9f0a89b7b362d09d38b81282e744329046
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-11 12:13:13 +0200

    feature: sort data for clean stepchart

--- frontend/src/services/data-drift.mappers.ts
+++ frontend/src/services/data-drift.mappers.ts
@@ -2,30 +2,59 @@ import {
   StepChartProps,
   YearMonthString,
 } from "../components/Charts/StepChart";
-import { CohortsMetricsMetadata } from "./data-drift.types";
+import { CohortPoint, CohortsMetricsMetadata } from "./data-drift.types";
 
 export const mapCohortsMetricsMetadataToStepChartProps = (
   cohortMetricMetadata: CohortsMetricsMetadata
 ): StepChartProps => {
-  const metricNames: StepChartProps["metricNames"] = Object.keys(
-    cohortMetricMetadata
-  ) as YearMonthString[];
+  const metricNames: StepChartProps["metricNames"] = [];
   const data: StepChartProps["data"] = [];
-  for (const metricName of metricNames) {
+  for (const metricName of Object.keys(
+    cohortMetricMetadata
+  ) as YearMonthString[]) {
+    // if (metricName !== "2023-02") continue;
     const cohort = cohortMetricMetadata[metricName];
-    const RelativeHistory = Object.keys(cohort.RelativeHistory);
+    const RelativeHistory = sortRelativeHistory(cohort.RelativeHistory);
+    console.log(cohort.RelativeHistory);
     if (RelativeHistory.length < 2) {
       continue;
     }
-    RelativeHistory.forEach((timeFromHistorisationInMs) => {
-      const cohortMetric = cohort.RelativeHistory[timeFromHistorisationInMs];
-      const datum = {
-        [metricName]: parseFloat(cohortMetric.RelativeValue),
-        daysSinceFirstReport: parseFloat(cohortMetric.DaysFromHistorization),
-      };
-      data.push(datum);
+    metricNames.push(metricName);
+    let latestKPI = undefined as number | undefined;
+    console.log(RelativeHistory);
+    RelativeHistory.sort();
+    console.log(RelativeHistory);
+
+    RelativeHistory.forEach((cohortMetric) => {
+      const currentKPI = parseFloat(cohortMetric.RelativeValue);
+      if (latestKPI === undefined || currentKPI != latestKPI) {
+        console.log("days", parseFloat(cohortMetric.DaysFromHistorization));
+        const datum = {
+          [metricName]: currentKPI,
+          daysSinceFirstReport: parseFloat(cohortMetric.DaysFromHistorization),
+        };
+        data.push(datum);
+        latestKPI = currentKPI;
+      }
     });
   }
+  console.log(data);
 
   return { data, metricNames };
 };
+
+function sortRelativeHistory(
+  cohort: Record<string, CohortPoint>
+): CohortPoint[] {
+  const relativeHistoryArray = Object.keys(cohort).map((key) => ({
+    ...cohort[key],
+    daysFromHistorisation: parseFloat(cohort[key].DaysFromHistorization),
+    relativeValue: parseFloat(cohort[key].RelativeValue),
+  }));
+
+  relativeHistoryArray.sort(
+    (a, b) => a.daysFromHistorisation - b.daysFromHistorisation
+  );
+
+  return relativeHistoryArray;
+}
