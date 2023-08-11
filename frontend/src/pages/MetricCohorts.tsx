ab4f844a8bfca8206f163db7a09a5a5dbc03f251
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-11 10:52:50 +0200

    feature: get route params in page

--- frontend/src/pages/MetricCohorts.tsx
+++ frontend/src/pages/MetricCohorts.tsx
@@ -1,16 +1,53 @@
-import { useLoaderData } from "react-router";
+import { Params, useLoaderData } from "react-router";
 import { StepChart, StepChartProps } from "../components/Charts/StepChart";
 
-const getMetricCohortsData = (): StepChartProps => {
+const getMetricCohortsData = ({
+  params,
+}: {
+  params: Params<string>;
+}): StepChartProps => {
+  const typedParams = assertParamsHasNeededProperties(params);
+  console.log(typedParams);
   return {
     metricNames: ["2022-01"],
-    data: [{ daysSinceFirstReport: 12, "2022-01": 12 }],
+    data: [
+      { daysSinceFirstReport: 12, "2022-01": 12 },
+      { daysSinceFirstReport: 13, "2022-01": 12 },
+    ],
   };
 };
 
+// Define the custom type
+type Timegrain = "quarter" | "month" | "week" | "day";
+
+// The assertion function
+function assertTimegrain(value: string): asserts value is Timegrain {
+  if (
+    value !== "quarter" &&
+    value !== "month" &&
+    value !== "week" &&
+    value !== "day"
+  ) {
+    throw new Error("Value is not a valid time unit!");
+  }
+}
+
+function assertParamsHasNeededProperties(params: Params<string>): {
+  installationId: string;
+  metricName: string;
+  timegrain: Timegrain;
+} {
+  const { installationId, metricName, timegrain } = params;
+  if (!installationId || !metricName || !timegrain) {
+    throw new Error("Invalid params");
+  }
+  assertTimegrain(timegrain);
+
+  return { installationId, metricName, timegrain };
+}
+
 const MetricCohort = () => {
   const { metricNames, data } = useLoaderData() as StepChartProps;
-  console.log(metricNames, data);
   return <StepChart metricNames={metricNames} data={data} />;
 };
 
--- frontend/src/services/data-drift.ts
+++ frontend/src/services/data-drift.ts
@@ -4,12 +4,10 @@ import { CommitInfo } from "../pages/DisplayCommit";
 export const getPatchAndHeader = async (
   params: CommitInfo & { installationId: string }
 ) => {
-  console.log("commitParams", params);
   const result = await axios.get<{ patch: string; headers: string[] }>(
     `https://data-drift.herokuapp.com/gh/${params.owner}/${params.repo}/commit/${params.commitSHA}`,
     { headers: { "Installation-Id": params.installationId } }
   );
-  console.log("result", result);
   return {
     patch: result.data.patch,
     headers: result.data.headers,
