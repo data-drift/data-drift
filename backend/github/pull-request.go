d59ebb31a052e8c6e3b3eaac56cb007bbb61a5ea
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-30 18:16:22 +0200

    bugfix: use head commit for diff url

--- backend/github/pull-request.go
+++ backend/github/pull-request.go
@@ -19,15 +19,15 @@ func handlePullRequestOpened(event *github.PullRequestEvent) error {
 	installationId := event.GetInstallation().GetID()
 	log.Printf("Pull request opened: owner=%s, repo=%s, installation_id=%d, number=%d, title=%s, url=%s", event.Repo.Owner.GetLogin(), event.Repo.GetName(), event.Installation.GetID(), event.PullRequest.GetNumber(), event.PullRequest.GetTitle(), event.PullRequest.GetHTMLURL())
 	// Get the pull request number from the event.
-	number := event.GetNumber()
 
-	commitDiffUrl := urlgen.BuildReportDiffUrl(urlgen.BuildReportDiffBaseUrl(fmt.Sprint(installationId), owner, repo), "036f9d6b685ee02a14faa70ed05e0bd60650c477")
+	commitDiffUrl := urlgen.BuildReportDiffUrl(urlgen.BuildReportDiffBaseUrl(fmt.Sprint(installationId), owner, repo), *event.PullRequest.Head.SHA)
 
 	// Create a comment on the pull request.
 	comment := &github.IssueComment{
 		Body: github.String(fmt.Sprintf("The diff is available [here](%s).", commitDiffUrl)),
 	}
 	client, _ := CreateClientFromGithubApp(*event.Installation.ID)
+	number := event.GetNumber()
 
 	_, _, err := client.Issues.CreateComment(context.Background(), owner, repo, number, comment)
 
