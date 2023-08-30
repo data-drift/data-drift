4242a947eb5e4136d7c63c283de0d7f6d29b3275
Sammy Teillet <sammy.teillet@gmail.com>
2023-08-30 14:36:23 +0200

    feature: add DD diff link to pullrequest

--- backend/github/pull-request.go
+++ backend/github/pull-request.go
@@ -2,7 +2,9 @@ package github
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/data-drift/data-drift/urlgen"
 	"github.com/google/go-github/v42/github"
 )
 
@@ -11,13 +13,16 @@ func handlePullRequestOpened(event *github.PullRequestEvent) error {
 	// Get the owner and repository name from the event.
 	owner := event.GetRepo().GetOwner().GetLogin()
 	repo := event.GetRepo().GetName()
+	installationId := event.GetInstallation().GetID()
 
 	// Get the pull request number from the event.
 	number := event.GetNumber()
 
+	commitDiffUrl := urlgen.BuildReportDiffUrl(urlgen.BuildReportDiffBaseUrl(fmt.Sprint(installationId), owner, repo), "036f9d6b685ee02a14faa70ed05e0bd60650c477")
+
 	// Create a comment on the pull request.
 	comment := &github.IssueComment{
-		Body: github.String("Thanks for opening this pull request!"),
+		Body: github.String(fmt.Sprintf("The diff is available [here](%s).", commitDiffUrl)),
 	}
 	client, _ := CreateClientFromGithubApp(*event.Installation.ID)
 
