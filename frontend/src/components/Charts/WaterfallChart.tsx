ec225fe95fd66a065caeac313141565358512cc8
Sammy Teillet <sammy.teillet@gmail.com>
2023-09-01 16:32:00 +0200

    feature: use auto scales

--- frontend/src/components/Charts/WaterfallChart.tsx
+++ frontend/src/components/Charts/WaterfallChart.tsx
@@ -1,7 +1,6 @@
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
 import { ComponentProps } from "react";
 
-// import type { Formatter } from "recharts";
 import { theme } from "../../theme";
 
 type TooltipProps = ComponentProps<typeof Tooltip<[number, number], "drift">>;
@@ -36,7 +35,7 @@ export const WaterfallChart = ({ data }: WaterfallChartProps) => {
       margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
     >
       <XAxis dataKey="day" />
-      <YAxis domain={[980, "dataMax"]} />
+      <YAxis type="number" domain={["auto", "auto"]} />
       <Tooltip
         contentStyle={{ backgroundColor: theme.colors.background }}
         formatter={formatToolTipValueTick}
