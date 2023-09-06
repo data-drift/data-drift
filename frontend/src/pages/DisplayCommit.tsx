c77263c38f8410dae2ff46a0d80fc496032737e1
Sammy Teillet <sammy.teillet@gmail.com>
2023-09-06 14:48:42 +0200

    Sammyt/95 aau i see the commit date metric name and GitHub link on my datadrift diff (#138)
    * feature: return date filename and link in gh route
    
    * refactor: rename type commitInfo
    
    * feature: display commit info on diff page
    
    * feature: add padding on commit info span
    
    * bugfix: set correct production url

--- frontend/src/pages/DisplayCommit.tsx
+++ frontend/src/pages/DisplayCommit.tsx
@@ -1,16 +1,17 @@
 import { getCommitFiles, getCsvHeaders } from "../services/github";
-import { DualTable, DualTableProps } from "../components/Table/DualTable";
+import { DualTable } from "../components/Table/DualTable";
 import { parsePatch } from "../services/patch.mapper";
 import { Params, useLoaderData } from "react-router";
 import { getPatchAndHeader } from "../services/data-drift";
+import styled from "@emotion/styled";
 
-export interface CommitInfo {
+export interface CommitParam {
   owner: string;
   repo: string;
   commitSHA: string;
 }
 
-function assertParamsIsCommitInfo(params: Params<string>): CommitInfo {
+function assertParamsIsCommitInfo(params: Params<string>): CommitParam {
   const { owner, repo, commitSHA } = params;
   if (!owner || !repo || !commitSHA) {
     throw new Error("Invalid params");
@@ -20,7 +21,7 @@ function assertParamsIsCommitInfo(params: Params<string>): CommitInfo {
 
 function assertParamsHasInstallationIs(
   params: Params<string>
-): CommitInfo & { installationId: string } {
+): CommitParam & { installationId: string } {
   const { installationId, owner, repo, commitSHA } = params;
   if (!installationId || !owner || !repo || !commitSHA) {
     throw new Error("Invalid params");
@@ -54,21 +55,42 @@ const getCommitDiffFromDataDrift = async ({
   const { installationId, owner, repo, commitSHA } =
     assertParamsHasInstallationIs(params);
 
-  const { patch, headers } = await getPatchAndHeader({
+  const { patch, headers, ...commitInfo } = await getPatchAndHeader({
     installationId,
     owner,
     repo,
     commitSHA,
   });
+
   const { oldData, newData } = parsePatch(patch, headers);
-  return { tableProps1: oldData, tableProps2: newData };
+  return { tableProps1: oldData, tableProps2: newData, commitInfo };
 };
 
-function DisplayCommit() {
-  const results = useLoaderData() as DualTableProps;
-  console.log("results", results);
+type LoaderData = Awaited<
+  ReturnType<typeof getCommitDiffFromGithub | typeof getCommitDiffFromDataDrift>
+>;
 
-  return <>{results && <DualTable {...results} />}</>;
+const StyledSpan = styled.span`
+  padding: 8px;
+`;
+
+function DisplayCommit() {
+  const results = useLoaderData() as LoaderData;
+
+  return (
+    <>
+      {results && "commitInfo" in results && (
+        <StyledSpan>
+          <b>
+            {results.commitInfo.filename}{" "}
+            {results.commitInfo.date.toLocaleDateString()}{" "}
+          </b>
+          <a href={results.commitInfo.commitLink}>commit</a>
+        </StyledSpan>
+      )}
+      {results && <DualTable {...results} />}
+    </>
+  );
 }
 
 DisplayCommit.githubLoader = getCommitDiffFromGithub;
