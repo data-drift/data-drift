051c2b4f1748cee90a6e7d3a775d1e41a9bf14da
Sammy Teillet <sammy.teillet@gmail.com>
2023-09-08 16:46:12 +0200

    feature: add new CommitListItem component

--- frontend/src/components/Commits/CommitListItem.stories.tsx
+++ frontend/src/components/Commits/CommitListItem.stories.tsx
@@ -10,4 +10,20 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const SimpleCase: Story = {};
+export const DriftCase: Story = {
+  args: {
+    type: "Drift",
+    date: new Date("2021-08-01T00:00:00Z"),
+    commitUrl: "www.google.com",
+    name: "New drift detected data/act_metrics_finance/bop_eop_mrr_monthly_by_country.csv (#76)\n\nDrift: data/act_metrics_finance/bop_eop_mrr_monthly_by_country.csv",
+  },
+};
+
+export const NewDataCase: Story = {
+  args: {
+    type: "New Data",
+    date: new Date("2021-08-01T00:00:00Z"),
+    commitUrl: "www.google.com",
+    name: "New drift detected data/act_metrics_finance/bop_eop_mrr_monthly_by_country.csv (#76)\n\nDrift: data/act_metrics_finance/bop_eop_mrr_monthly_by_country.csv",
+  },
+};
--- frontend/src/components/Commits/CommitListItem.tsx
+++ frontend/src/components/Commits/CommitListItem.tsx
@@ -1,3 +1,60 @@
-export const CommitListItem = () => {
-  return <div>Commit List Item</div>;
+type CommitListItemProps = {
+  type: "Drift" | "New Data";
+  date: Date;
+  name: string;
+  commitUrl: string;
+};
+
+export const CommitListItem = ({
+  type,
+  date,
+  name,
+  commitUrl,
+}: CommitListItemProps) => {
+  return (
+    <div
+      style={{
+        border: "1px solid #ccc",
+        padding: "16px",
+        borderRadius: "0",
+        marginBottom: "16px",
+      }}
+    >
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+          <span
+            style={{
+              backgroundColor: type === "Drift" ? "#e0e0e0" : "#a5d6a7",
+              color: type === "Drift" ? "#000" : "#fff",
+              borderRadius: "0",
+              padding: "4px 8px",
+              fontWeight: "bold",
+            }}
+          >
+            {type}
+          </span>
+        </div>
+        <span style={{ color: "#888" }}>{date.toLocaleString()}</span>
+      </div>
+      <div style={{ marginTop: "8px" }}>
+        <p
+          dangerouslySetInnerHTML={{ __html: name.replace(/\n/g, "<br/>") }}
+        ></p>
+      </div>
+      <a href={commitUrl} target="_blank" rel="noopener noreferrer">
+        <button
+          style={{
+            padding: "8px 16px",
+            backgroundColor: "#333",
+            color: "#fff",
+            borderRadius: "0px",
+            border: "2px solid #fff",
+            fontFamily: "monospace",
+          }}
+        >
+          View Commit
+        </button>
+      </a>
+    </div>
+  );
 };
