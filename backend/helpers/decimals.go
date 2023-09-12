5a65ce47658b9f614ab3dc4f441ce77e5fc631d1
Sammy Teillet <sammy.teillet@gmail.com>
2023-09-12 15:22:45 +0200

    feature: add method to format numbers with thousands separator

--- backend/helpers/decimals.go
+++ backend/helpers/decimals.go
@@ -1,8 +1,39 @@
 package helpers
 
-import "github.com/shopspring/decimal"
+import (
+	"strings"
+
+	"github.com/shopspring/decimal"
+)
 
 func GetFloat(dec decimal.Decimal) float64 {
 	f, _ := dec.Float64()
 	return f
+}
+
+func FormatWithSeparator(d decimal.Decimal) string {
+	isNegative := d.IsNegative()
+	whole := d.Abs().Floor()
+	frac := d.Abs().Sub(whole)
+
+	var sb strings.Builder
+	wholeLen := len(whole.String())
+
+	if isNegative {
+		sb.WriteRune('-')
+	}
+
+	for i, digit := range whole.String() {
+		sb.WriteRune(digit)
+		if (wholeLen-i-1)%3 == 0 && i < wholeLen-1 {
+			sb.WriteRune(',')
+		}
+	}
+
+	if !frac.IsZero() {
+		sb.WriteRune('.')
+		sb.WriteString(frac.String()[2:])
+	}
+
+	return sb.String()
 }
---
+++ backend/helpers/decimals_test.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"testing"
+
+	"github.com/shopspring/decimal"
+)
+
+func TestFormatWithSeparator(t *testing.T) {
+	testCases := []struct {
+		input decimal.Decimal
+		want  string
+	}{
+		{decimal.NewFromFloat(1234567.89), "1,234,567.89"},
+		{decimal.NewFromFloat(0), "0"},
+		{decimal.NewFromFloat(-1234567.89), "-1,234,567.89"},
+		{decimal.NewFromFloat(0.123456789), "0.123456789"},
+		{decimal.NewFromFloat(0.000000001), "0.000000001"},
+	}
+
+	for _, tc := range testCases {
+		got := FormatWithSeparator(tc.input)
+		if got != tc.want {
+			t.Errorf("FormatWithSeparator(%v) = %v; want %v", tc.input, got, tc.want)
+		}
+	}
+}
