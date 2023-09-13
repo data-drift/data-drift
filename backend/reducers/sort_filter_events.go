b5218651f361b6910e8337f161cbd388742187f0
Sammy Teillet <sammy.teillet@gmail.com>
2023-09-13 17:52:26 +0200

    refactor: rename lastDate to first date

--- backend/reducers/sort_filter_events.go
+++ backend/reducers/sort_filter_events.go
@@ -32,32 +32,32 @@ func FilterAndSortByCommitTimestamp[T ObjectWithDate](dataSortableArray []T, dri
 func GetFirstDateOfPeriod(periodKeyParam common.PeriodKey) (time.Time, error) {
 	timegrain, timeGrainError := reports.GetTimeGrain(periodKeyParam)
 	periodKey := string(periodKeyParam)
-	var lastDay time.Time
+	var firstDate time.Time
 	if timeGrainError != nil {
 		fmt.Println("Error:", timeGrainError.Error())
-		return lastDay, timeGrainError
+		return firstDate, timeGrainError
 	}
 	switch timegrain {
 	case common.Day:
-		lastDay, _ = time.Parse("2006-01-02", periodKey)
+		firstDate, _ = time.Parse("2006-01-02", periodKey)
 	case common.Week:
 		periodTime, _ := reports.ParseYearWeek(periodKey)
-		lastDay = periodTime.AddDate(0, 0, 6).Add(time.Duration(23)*time.Hour + time.Duration(59)*time.Minute + time.Duration(59)*time.Second)
+		firstDate = periodTime.AddDate(0, 0, 6).Add(time.Duration(23)*time.Hour + time.Duration(59)*time.Minute + time.Duration(59)*time.Second)
 	case common.Month:
 		periodTime, _ := time.Parse("2006-01", periodKey)
 
-		lastDay = periodTime.AddDate(0, 1, -1).Add(time.Duration(23)*time.Hour + time.Duration(59)*time.Minute + time.Duration(59)*time.Second)
+		firstDate = periodTime.AddDate(0, 1, -1).Add(time.Duration(23)*time.Hour + time.Duration(59)*time.Minute + time.Duration(59)*time.Second)
 	case common.Quarter:
 		periodTime, _ := reports.ParseQuarterDate(periodKey)
 
-		lastDay = periodTime
+		firstDate = periodTime
 	case common.Year:
 		periodTime, _ := time.Parse("2006", periodKey)
-		lastDay = time.Date(periodTime.Year(), 12, 31, 23, 59, 59, 0, time.UTC)
+		firstDate = time.Date(periodTime.Year(), 12, 31, 23, 59, 59, 0, time.UTC)
 	default:
 		fmt.Printf("Invalid time grain: %s", timegrain)
-		return lastDay, fmt.Errorf("invalid time grain: %s", timegrain)
+		return firstDate, fmt.Errorf("invalid time grain: %s", timegrain)
 	}
-	return lastDay, nil
+	return firstDate, nil
 
 }
