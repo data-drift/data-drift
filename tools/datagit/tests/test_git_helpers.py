ffcc9a4531a01c46f8bf4eda33dee9fc29acb5c7
Sammy Teillet <sammy.teillet@gmail.com>
2023-09-20 15:08:33 +0200

    test: update drift branch name in test

--- tools/datagit/tests/test_git_helpers.py
+++ tools/datagit/tests/test_git_helpers.py
@@ -1,18 +1,22 @@
 import unittest
 from unittest.mock import MagicMock
 from datagit.github_connector import get_valid_branch_name, create_git_branch
+import re
 
 
 class TestGetValidBranchName(unittest.TestCase):
     def test_valid_branch_name(self):
         filepath = "/path/to/my/file.txt"
-        expected_branch_name = "metric/path-to-my-file-txt"
-        self.assertEqual(get_valid_branch_name(filepath), expected_branch_name)
+        pattern = r"drift/\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}/path-to-my-file-txt"
+        assert re.match(pattern, get_valid_branch_name(filepath))
 
     def test_invalid_branch_name(self):
         filepath = "/path/to/my/file with spaces.txt"
-        expected_branch_name = "metric/path-to-my-file-with-spaces-txt"
-        self.assertEqual(get_valid_branch_name(filepath), expected_branch_name)
+        pattern = (
+            r"drift/\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}/path-to-my-file-with-spaces-txt"
+        )
+
+        assert re.match(pattern, get_valid_branch_name(filepath))
 
 
 class TestGithubConnector(unittest.TestCase):
