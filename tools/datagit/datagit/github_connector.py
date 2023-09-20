eeeaff616f1e22a9b0442f8014e3d6c3db46fd93
Sammy Teillet <sammy.teillet@gmail.com>
2023-09-20 10:44:25 +0200

    feature: add partition method

--- tools/datagit/datagit/github_connector.py
+++ tools/datagit/datagit/github_connector.py
@@ -71,6 +71,60 @@ def store_metric(
     )
 
 
+def partition_and_store_metric(
+    ghClient: Github,
+    dataframe: pd.DataFrame,
+    filepath: str,
+    assignees: Optional[List[str]] = None,
+    store_json: bool = False,
+    drift_evaluator: Callable[
+        [Dict[str, pd.DataFrame]], Dict
+    ] = default_drift_evaluator,
+) -> None:
+    """
+    Store metrics into a specific repository file on GitHub.
+
+    Parameters:
+      ghClient (Github): An instance of a GitHub client to interact with the GitHub API.
+      dataframe (pd.DataFrame): The dataframe containing the metrics to be stored.
+      filepath (str): The full path to the target file in the format
+        'organization/repository/path_to_file'.
+      assignees (List[str]): List of GitHub usernames to be assigned to the pull request.
+        Defaults to an empty list. If list is empty no alert will be raised, nor pull
+        request will be created.
+      branch (Optional[str]): The name of the branch where the metrics will be stored.
+        If None, a branch name will be generated f"metric/{filepath}". Defaults to None.
+      store_json (bool): If True, stores the dataframe in the .json format.
+        Defaults to True.
+      drift_evaluator (Callable): Function that evaluates context and return information
+        about how drift should be handled. See `drift_evaluator` module.
+
+    Returns:
+      None: This function does not return any value, but it performs a side effect of
+      pushing the metric to GitHub.
+
+    Raises:
+      ValueError: If the dataframe does not have a unique first column or if the file
+        path does not have the format 'organization/repository/path_to_file'.
+      GithubException: If there is an error in interacting with the GitHub API, e.g.,
+        insufficient permissions, non-existent repo, etc.
+    """
+
+    print("Partitionning metric...")
+
+    dataframe["date"] = pd.to_datetime(dataframe["date"])
+
+    grouped = dataframe.groupby(pd.Grouper(key="date", freq="M"))
+
+    # Iterate over the groups and print the sub-dataframes
+    for name, group in grouped:
+        print(f"Storing metric for Month: {name}")
+        monthly_filepath = get_monthly_file_path(filepath, name.strftime("%Y-%m"))
+        store_metric(
+            ghClient, group, monthly_filepath, assignees, store_json, drift_evaluator
+        )
+
+
 def push_metric(
     dataframe,
     assignees,
