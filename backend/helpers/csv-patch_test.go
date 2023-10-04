9c05f5135436b9f70934e5c22cafbc07becd3a50
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-04 17:36:52 +0200

    test: fix patch tests

--- backend/helpers/csv-patch_test.go
+++ backend/helpers/csv-patch_test.go
@@ -42,7 +42,7 @@ func TestGenerateCsvPatch(t *testing.T) {
 	}
 
 	// Check that the patch string is correct
-	expectedPatch := "@@ -2,9 +2,9 @@\n 2022-12-Alice,Alice,2022-12,25\n 2023-01-Bob,Bob,2023-01,30\n 2023-01-Charlie,Charlie,2023-01,36\n+2023-01-Charline,Charline,2023-01,42\n 2023-02-Antoine,Antoine,2023-02,40\n-2023-02-Didier,Didier,2023-02,40\n+2023-02-Didou,Didou,2023-02,40\n 2023-02-Philipe,Philipe,2023-02,42\n-2023-03-Clement,Clement,2023-03,45\n 2023-03-Cyril,Cyril,2023-03,45\n 2023-03-Victor,Victor,2023-03,46"
+	expectedPatch := "@@ -1,10 +1,10 @@\n-unique_key,name,date,age \n+unique_key,name,date,age\n 2022-12-Alice,Alice,2022-12,25\n 2023-01-Bob,Bob,2023-01,30\n 2023-01-Charlie,Charlie,2023-01,36\n+2023-01-Charline,Charline,2023-01,42\n 2023-02-Antoine,Antoine,2023-02,40\n-2023-02-Didier,Didier,2023-02,40\n+2023-02-Didou,Didou,2023-02,40\n 2023-02-Philipe,Philipe,2023-02,42\n-2023-03-Clement,Clement,2023-03,45\n 2023-03-Cyril,Cyril,2023-03,45\n 2023-03-Victor,Victor,2023-03,46"
 
 	for i, line := range strings.Split(patch, "\n") {
 		if line != strings.Split(expectedPatch, "\n")[i] {
