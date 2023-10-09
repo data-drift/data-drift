a87571eea25ab3d931469af52d6e6b7dfdb72d96
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-09 10:32:37 +0200

    refactor: extract component in file

--- frontend/src/pages/Overview/Overview.tsx
+++ frontend/src/pages/Overview/Overview.tsx
@@ -1,76 +1,19 @@
-import styled from "@emotion/styled";
 import Lineage from "../../components/Lineage/Lineage";
 import { Position } from "reactflow";
 import DualTableHeader from "../../components/Table/DualTableHeader";
 import { DualTable } from "../../components/Table/DualTable";
 import { useCallback, useState } from "react";
-
-const Container = styled.div`
-  width: 100%;
-  box-sizing: border-box;
-`;
-
-const StyledHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: ${(props) =>
-    props.theme.colors.background2}; // muted background color
-  padding: 20px 40px;
-  width: 100%;
-  box-sizing: border-box;
-`;
-
-const StyledDate = styled.div`
-  font-size: 32px;
-  font-weight: bold;
-  color: ${(props) => props.theme.colors.text};
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 4px;
-`;
-
-const StyledSelect = styled.select`
-  background-color: ${(props) => props.theme.colors.background};
-  border: none;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-`;
-
-const StyledDateButton = styled.button`
-  background-color: transparent;
-  color: ${(props) => props.theme.colors.text2};
-  border: 0;
-  border-radius: 0;
-  padding: 5px 10px;
-  font-size: 18px;
-  cursor: pointer;
-  &:hover {
-    background-color: #bbb; // or any other color indication for hover
-  }
-`;
-
-const LineageContainer = styled.div`
-  background-color: ${(props) => props.theme.colors.background2};
-  text-align: left;
-`;
-
-const StyledCollapsibleTitle = styled.button`
-  cursor: pointer;
-  padding: 10px;
-  border: none;
-  background-color: ${(props) => props.theme.colors.background2};
-`;
-
-const StyledCollapsibleContent = styled.div<{ isCollapsed: boolean }>`
-  height: ${(props) => (props.isCollapsed ? "0" : "300px")};
-
-  overflow: hidden;
-`;
-
-const DiffTableContainer = styled.div``;
+import {
+  Container,
+  DiffTableContainer,
+  LineageContainer,
+  StyledCollapsibleContent,
+  StyledCollapsibleTitle,
+  StyledDate,
+  StyledDateButton,
+  StyledHeader,
+  StyledSelect,
+} from "./components";
 
 const nodes = [
   {
