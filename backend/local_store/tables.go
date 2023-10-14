894582135948c6220a15cfeca8f27399cad99a3b
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-14 17:30:39 +0200

    feature: get tables

--- backend/local_store/tables.go
+++ backend/local_store/tables.go
@@ -1,12 +1,60 @@
 package local_store
 
 import (
+	"fmt"
 	"net/http"
+	"os/user"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"gopkg.in/src-d/go-git.v4"
 )
 
 func TablesHandler(c *gin.Context) {
 	store := c.Param("store")
-	c.JSON(http.StatusOK, store)
+	tables := getListOfFilesFromStore(store)
+	c.JSON(http.StatusOK, tables)
+}
+
+func getListOfFilesFromStore(store string) []string {
+	repoDir, err := getStoreDir(store)
+	if err != nil {
+		fmt.Println("Error getting store directory:", err)
+		return nil
+	}
+
+	repo, err := git.PlainOpen(repoDir)
+	if err != nil {
+		fmt.Println("Error opening repo:", err)
+		return nil
+	}
+
+	worktree, err := repo.Worktree()
+	if err != nil {
+		fmt.Println("Error getting worktree:", err)
+		return nil
+	}
+	files, err := worktree.Filesystem.ReadDir(".")
+	if err != nil {
+		fmt.Println("Error getting files:", err)
+		return nil
+	}
+	fileNames := []string{}
+
+	for _, file := range files {
+		fileNames = append(fileNames, file.Name())
+	}
+
+	return fileNames
+}
+
+func getStoreDir(store string) (string, error) {
+	currentUser, err := user.Current()
+	if err != nil {
+		return "", nil
+	}
+	homeDir := currentUser.HomeDir
+
+	repoDir := filepath.Join(homeDir, ".datadrift", store)
+	return repoDir, nil
 }
