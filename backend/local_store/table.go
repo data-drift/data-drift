dcc614b8feca065ff6089e7f0d14b89ac59f99c8
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-17 11:13:51 +0200

    feature: add commit info to table url

--- backend/local_store/table.go
+++ backend/local_store/table.go
@@ -6,8 +6,11 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
 func TableHandler(c *gin.Context) {
@@ -20,12 +23,25 @@ func TableHandler(c *gin.Context) {
 		})
 		return
 	}
-	print(tableColumns)
+	commits, err := getCommitsForFile(store, table+".csv")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	print(commits)
 	c.JSON(http.StatusOK, gin.H{
 		"store":        store,
 		"table":        table,
 		"tableColumns": tableColumns,
+		"commits":      commits,
 	})
+}
+
+type CommitInfo struct {
+	Message string
+	Date    time.Time
 }
 
 func getListOfColumnsFromTable(store string, table string) ([]string, error) {
@@ -48,5 +64,59 @@ func getListOfColumnsFromTable(store string, table string) ([]string, error) {
 		return nil, err
 	}
 	headers := records[0]
+
 	return headers, nil
+}
+
+func getCommitsForFile(store string, filePath string) ([]CommitInfo, error) {
+	repoDir, err := getStoreDir(store)
+	if err != nil {
+		print("Error getting store directory")
+		return nil, err
+	}
+	repo, err := git.PlainOpen(repoDir)
+	if err != nil {
+		print("Error opening repo")
+		return nil, err
+	}
+
+	// Get the HEAD reference
+	ref, err := repo.Head()
+	if err != nil {
+		print("Error getting HEAD reference")
+		return nil, err
+	}
+
+	// Get the commit history for the file
+	commitIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		print("Error getting commit history")
+		return nil, err
+	}
+
+	// Filter the commit history to only include commits that modified the file
+	var commits []CommitInfo
+	err = commitIter.ForEach(func(commit *object.Commit) error {
+		// Check if the commit modified the file
+		tree, err := commit.Tree()
+		if err != nil {
+			print("Error getting tree")
+			return err
+		}
+		_, err = tree.File(filePath)
+		if err == nil {
+			commits = append(commits, CommitInfo{
+				Message: commit.Message,
+				Date:    commit.Author.When,
+			})
+		}
+
+		return nil
+	})
+	if err != nil {
+		print("Error iterating commits")
+		return nil, err
+	}
+
+	return commits, nil
 }
