291e4b9ab332a43efdfa8181f59ace34486458d0
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-19 14:06:57 +0200

    feature: return measurement metadata

--- backend/local_store/measurement.go
+++ backend/local_store/measurement.go
@@ -1,11 +1,14 @@
 package local_store
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/data-drift/data-drift/common"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
 type MeasurementRequest struct {
@@ -15,6 +18,7 @@ type MeasurementRequest struct {
 
 func MeasurementHandler(c *gin.Context) {
 	store := c.Param("store")
+	table := c.Param("table")
 	measurementId := c.Param("measurementId")
 
 	var req MeasurementRequest
@@ -23,6 +27,57 @@ func MeasurementHandler(c *gin.Context) {
 		return
 	}
 
-	log.Println(store, measurementId, req.TimeGrain)
-	// get the record, compute every period in the timegrain
+	commit, err := getMeasurement(store, table, measurementId)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	measurementMetaData := common.MeasurementMetaData{
+
+		MeasurementTimestamp: commit.Author.When.Unix(),
+		MeasurementDate:      commit.Author.When.Format("2006-01-02"),
+		MeasurementDateTime:  commit.Author.When.Format("2006-01-02 15:04:05"),
+		MeasurementId:        measurementId,
+		MeasurementComments: []common.CommitComments{
+			{
+				CommentAuthor: commit.Author.Name,
+				CommentBody:   commit.Message,
+			},
+		},
+	}
+
+	c.JSON(http.StatusOK, gin.H{"MeasurementMetaData": measurementMetaData})
+
+}
+
+func getMeasurement(store string, table string, commitSha string) (*object.Commit, error) {
+	repoDir, err := getStoreDir(store)
+	filePath := table + ".csv"
+	if err != nil {
+		print("Error getting store directory")
+		return nil, err
+	}
+	repo, err := git.PlainOpen(repoDir)
+	if err != nil {
+		print("Error opening repo")
+		return nil, err
+	}
+
+	if err != nil {
+		print("Error getting HEAD reference")
+		return nil, err
+	}
+	hash := plumbing.NewHash(commitSha)
+
+	commit, err := repo.CommitObject(hash)
+	if err != nil {
+		return nil, err
+	}
+	_, err = commit.File(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("file not present in measurement")
+	}
+	return commit, err
 }
