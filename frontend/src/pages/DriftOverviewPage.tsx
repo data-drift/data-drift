7d99ad7d41452773fe0793d4e1800c65133db790
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-20 15:40:36 +0200

    feature: add detail container

--- frontend/src/pages/DriftOverviewPage.tsx
+++ frontend/src/pages/DriftOverviewPage.tsx
@@ -23,6 +23,34 @@ const DriftDetailContainer = styled.div`
   padding: ${({ theme }) => theme.spacing(2)};
   box-sizing: border-box;
   clip-path: ${({ theme }) => theme.upLeftClipping};
+  display: flex;
+  flex-direction: row;
+  gap: ${({ theme }) => theme.spacing(20)};
+`;
+
+const MetadataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin-bottom: ${({ theme }) => theme.spacing(2)};
+`;
+
+const BlackContainer = styled.div`
+  width: 100%;
+  background-color: black;
+  padding: ${({ theme }) => theme.spacing(2)};
+  box-sizing: border-box;
+  margin-top: ${({ theme }) => theme.spacing(2)};
+  text-align: start;
+`;
+
+const TansparentContainer = styled.div`
+  width: 100%;
+  padding: ${({ theme }) => theme.spacing(2)} 0;
+  box-sizing: border-box;
+  margin-top: ${({ theme }) => theme.spacing(2)};
+  text-align: start;
 `;
 
 const DriftOverviewPage = () => {
@@ -30,7 +58,23 @@ const DriftOverviewPage = () => {
     <PageContainer>
       <h1>Drift overview</h1>
       <Separator />
-      <DriftDetailContainer>Coucou</DriftDetailContainer>
+      <DriftDetailContainer>
+        <MetadataContainer>
+          <strong>Metric Metadata</strong>
+          <BlackContainer>Stabilization time: 3d</BlackContainer>
+          <BlackContainer>Volatility: 1%</BlackContainer>
+        </MetadataContainer>
+        <MetadataContainer>
+          <strong>Current Drift</strong>
+          <TansparentContainer>
+            Detected on {new Date().toLocaleDateString()}
+          </TansparentContainer>
+          <TansparentContainer>4 month impacted</TansparentContainer>
+          <BlackContainer>
+            Total drift <strong>48.9</strong>
+          </BlackContainer>
+        </MetadataContainer>
+      </DriftDetailContainer>
     </PageContainer>
   );
 };
