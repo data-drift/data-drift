9ca2921f50d40af984bdab47c6557fc7e90215c9
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-23 16:27:59 +0200

    feature: add margin on top to make sure the trend chip is not cropped

--- frontend/src/components/Charts/DualMetricBarChart.tsx
+++ frontend/src/components/Charts/DualMetricBarChart.tsx
@@ -113,7 +113,7 @@ const DualMetricBarChart = ({ data }: Props) => {
     <ResponsiveContainer width="100%" height={300}>
       <BarChart
         data={data}
-        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        margin={{ top: 48, right: 30, left: 20, bottom: 5 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
