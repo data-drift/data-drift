219caac049a730e680b473f6a299731c62c3d98c
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-27 15:37:08 +0200

    Sammyt/cli dbt snapshot (#227)
    * feature: add index in list selection
    
    * refactor: remove warning from wring typing
    
    * feature: orient cli to work for 1 snapshot only
    
    * feature: add check if server already running
    
    * feature: move start server method to another file

--- tools/datagit/datagit/cli.py
+++ tools/datagit/datagit/cli.py
@@ -1,8 +1,7 @@
-import sys
-import webbrowser
 import click
 import json
 from datagit.dataset import generate_dataframe, insert_drift
+from datagit.server import start_server
 import numpy as np
 import pandas as pd
 import os
@@ -10,12 +9,9 @@ from datagit import github_connector
 from datagit import local_connector
 from datagit.drift_evaluators import auto_merge_drift
 from github import Github
-import subprocess
-import platform
+
 from datetime import datetime
-import pkg_resources
-import http.server
-import socketserver
+
 from tzlocal import get_localzone
 
 
@@ -80,7 +76,7 @@ def run(token, repo, storage, project_dir):
 
     for node in data_drift_nodes:
         query = f'SELECT {node["config"]["meta"]["datadrift_unique_key"]} as unique_key,{node["config"]["meta"]["datadrift_date"]} as date, * FROM {node["relation_name"]}'
-        with adapter.connection_named("default"):
+        with adapter.connection_named("default"):  # type: ignore
             dataframe = dbt_adapter_query(adapter, query)
 
             if storage == "github":
@@ -122,134 +118,86 @@ def snapshot():
         if (node["resource_type"] == "snapshot")
     ]
 
-    for node in snapshot_nodes:
-        print("Handling node:", node["unique_id"])
-        with adapter.connection_named("default"):
-            snapshot_table = node["relation_name"]
-            date_column = node["config"]["meta"]["datadrift_date"]
-            unique_key = node["config"]["unique_key"]
-            metric_name = node["name"]
+    [snapshot_name, snapshot_index] = select_from_list(
+        "Which snapshot do you want to process?",
+        [node["name"] for node in snapshot_nodes],
+    )
 
-            text_query = f"""
-            SELECT DISTINCT dbt_valid_from FROM {snapshot_table}
-            UNION
-            SELECT DISTINCT dbt_valid_to FROM {snapshot_table} WHERE NOT NULL;
+    node = snapshot_nodes[snapshot_index]
+    print("Handling node:", node["unique_id"])
+
+    snapshot_table = node["relation_name"]
+    default_date_column = "created_at"
+
+    # prompt the user for a date column
+    date_column = click.prompt(
+        "Enter the date column name", default=default_date_column
+    )
+
+    unique_key = node["config"]["unique_key"]
+    metric_name = node["name"]
+
+    with adapter.connection_named("default"):  # type: ignore
+        text_query = f"""
+        SELECT DISTINCT dbt_valid_from FROM {snapshot_table}
+        UNION
+        SELECT DISTINCT dbt_valid_to FROM {snapshot_table} WHERE NOT NULL;
+        """
+
+        df = dbt_adapter_query(adapter, text_query)
+        df["dbt_valid_from"] = pd.to_datetime(df["dbt_valid_from"])
+
+        metric_history = local_connector.get_metric_history(metric_name=metric_name)
+        latest_commit = next(metric_history, None)
+        if latest_commit is not None:
+            authored_date = latest_commit.authored_date
+            authored_datetime = datetime.fromtimestamp(authored_date)
+            # Compare only the second part, without the milliseconds, and take dates after the latest measure commit
+            df = df.loc[df["dbt_valid_from"].dt.floor("S") > authored_datetime]
+
+        print("Snapshot dates to process:", df)
+
+        for index, row in df.iterrows():
+            date = row["dbt_valid_from"]
+            print(f"Processing data for date: {date}")
+            if pd.isna(date):
+                date = pd.Timestamp.now()
+
+            asOfQuery = f"""
+            SELECT * FROM {snapshot_table}
+            WHERE ('{date}' >= dbt_valid_from AND  '{date}' < dbt_valid_to) OR
+                (dbt_valid_to IS NULL AND dbt_valid_from <= '{date}');
             """
 
-            df = dbt_adapter_query(adapter, text_query)
-            df["dbt_valid_from"] = pd.to_datetime(df["dbt_valid_from"])
+            data_as_of_date = dbt_adapter_query(adapter, asOfQuery)
 
-            metric_history = local_connector.get_metric_history(metric_name=metric_name)
-            latest_commit = next(metric_history, None)
-            if latest_commit is not None:
-                authored_date = latest_commit.authored_date
-                authored_datetime = datetime.fromtimestamp(authored_date)
-                # Compare only the second part, without the milliseconds, and take dates after the latest measure commit
-                df = df.loc[df["dbt_valid_from"].dt.floor("S") > authored_datetime]
+            data_as_of_date.replace({np.nan: "NA"}, inplace=True)
 
-            print("Snapshot dates to process:", df)
+            # Drop dbt columns and add date and unique_key columns
+            data_as_of_date = data_as_of_date.drop(
+                ["dbt_scd_id", "dbt_updated_at", "dbt_valid_from", "dbt_valid_to"],
+                axis=1,
+            )
+            data_as_of_date["date"] = data_as_of_date[date_column]
+            data_as_of_date["unique_key"] = data_as_of_date[unique_key]
+            print(data_as_of_date)
 
-            for index, row in df.iterrows():
-                date = row["dbt_valid_from"]
-                print(f"Processing data for date: {date}")
-                if pd.isna(date):
-                    date = pd.Timestamp.now()
+            # Compute date in UTC format
+            local_tz = get_localzone()
+            localized_date = date.replace(tzinfo=local_tz)
 
-                asOfQuery = f"""
-                SELECT * FROM {snapshot_table}
-                WHERE ('{date}' >= dbt_valid_from AND  '{date}' < dbt_valid_to) OR
-                    (dbt_valid_to IS NULL AND dbt_valid_from <= '{date}');
-                """
-
-                data_as_of_date = dbt_adapter_query(adapter, asOfQuery)
-
-                data_as_of_date.replace({np.nan: "NA"}, inplace=True)
-
-                # Drop dbt columns and add date and unique_key columns
-                data_as_of_date = data_as_of_date.drop(
-                    ["dbt_scd_id", "dbt_updated_at", "dbt_valid_from", "dbt_valid_to"],
-                    axis=1,
-                )
-                data_as_of_date["date"] = data_as_of_date[date_column]
-                data_as_of_date["unique_key"] = data_as_of_date[unique_key]
-                print(data_as_of_date)
-
-                # Compute date in UTC format
-                local_tz = get_localzone()
-                localized_date = date.replace(tzinfo=local_tz)
-
-                local_connector.store_metric(
-                    metric_name=metric_name,
-                    metric_value=data_as_of_date,
-                    measure_date=localized_date,
-                )
+            local_connector.store_metric(
+                metric_name=metric_name,
+                metric_value=data_as_of_date,
+                measure_date=localized_date,
+            )
+    start_server("/tables/" + metric_name)
 
 
 @cli_entrypoint.command()
 def start():
     click.echo("Starting the application...")
-
-    if platform.system() == "Darwin":
-        if platform.machine().startswith("arm"):
-            binary_path = pkg_resources.resource_filename(
-                "datagit", "bin/data-drift-mac-m1"
-            )
-        else:
-            binary_path = pkg_resources.resource_filename(
-                "datagit", "bin/data-drift-mac-intel"
-            )
-    else:
-        # TODO: Update this path for other platforms (Linux, Windows, etc.)
-        raise ValueError("Unsupported platform")
-
-    # Get a copy of the current environment variables
-    env = os.environ.copy()
-
-    # Set the PORT environment variable
-    env["PORT"] = "9740"
-
-    server_process = subprocess.Popen(
-        [binary_path],
-        env=env,
-    )
-
-    PORT = 9741
-
-    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-    DIRECTORY = os.path.join(SCRIPT_DIR, "bin/frontend/dist")
-
-    class Handler(http.server.SimpleHTTPRequestHandler):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, directory=DIRECTORY, **kwargs)
-
-        def do_GET(self):
-            print(f"Request path: {self.path}")
-            # If the requested URL maps to an existing file, serve that.
-            if os.path.exists(self.translate_path(self.path)):
-                super().do_GET()
-                return
-
-            # Otherwise, serve the main index.html file.
-            self.path = "index.html"
-            super().do_GET()
-
-    httpd = socketserver.TCPServer(("", PORT), Handler)
-
-    try:
-        print(f"Serving directory '{DIRECTORY}' on port {PORT}")
-        url = f"http://localhost:{PORT}/tables"
-        print("Opening browser...", url)
-        webbrowser.open(url)
-        httpd.serve_forever()
-        server_process.wait()
-
-    except KeyboardInterrupt:
-        click.echo("Shutting down servers...")
-        httpd.shutdown()
-        click.echo("Httpd shut down")
-        server_process.terminate()
-        click.echo("Server down")
-        sys.exit()
+    start_server()
 
 
 @cli_entrypoint.group()
@@ -290,7 +238,7 @@ def create(table, row_number):
 def update(table, row_number):
     if not table:
         tables = local_connector.get_metrics()
-        table = select_from_list("Please enter table number", tables)
+        [table] = select_from_list("Please enter table number", tables)
 
     click.echo("Updating seed file...")
     dataframe = local_connector.get_metric(metric_name=table)
@@ -307,7 +255,7 @@ def delete(table):
     tables = local_connector.get_metrics()
     if not table:
         tables = local_connector.get_metrics()
-        table = select_from_list("Please enter table number", tables)
+        [table] = select_from_list("Please enter table number", tables)
     local_connector.delete_metric(metric_name=table)
 
 
@@ -340,7 +288,7 @@ def load_csv(csvpathfile, table, unique_key_column, date_column):
         if not unique_key_column:
             unique_key_column = click.prompt(
                 "Please enter unique key column name",
-                type=click.Choice(dataframe.columns),
+                type=click.Choice(dataframe.columns),  # type: ignore
             )
 
         assert (
@@ -351,7 +299,7 @@ def load_csv(csvpathfile, table, unique_key_column, date_column):
     if "date" not in dataframe.columns:
         if not date_column:
             date_column = click.prompt(
-                "Please enter date column name", type=click.Choice(dataframe.columns)
+                "Please enter date column name", type=click.Choice(dataframe.columns)  # type: ignore
             )
         assert (
             date_column in dataframe.columns
@@ -365,7 +313,8 @@ def select_from_list(prompt, choices):
     for idx, item in enumerate(choices, 1):
         click.echo(f"{idx}: {item}")
     selection = click.prompt(prompt, type=click.IntRange(1, len(choices)))
-    return choices[selection - 1]
+    index = selection - 1
+    return [choices[index], index]
 
 
 def dbt_adapter_query(
