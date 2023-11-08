6c15dda9b5cb6f0bada9c78c3aa11425d5e09762
Sammy Teillet <sammy.teillet@gmail.com>
2023-11-08 17:27:43 +0100

    refactor: remove deprecated store_json parameter

--- tools/datagit/datagit/github_connector.py
+++ tools/datagit/datagit/github_connector.py
@@ -23,7 +23,6 @@ def store_metric(
     filepath: str,
     branch: Optional[str] = None,
     assignees: Optional[List[str]] = None,
-    store_json: bool = False,
     drift_evaluator: Callable[[DriftEvaluatorContext], Dict] = auto_merge_drift,
 ) -> None:
     """
@@ -39,8 +38,6 @@ def store_metric(
         request will be created.
       branch (Optional[str]): The name of the branch where the metrics will be stored.
         If None, the default branch will be used. Defaults to None.
-      store_json (bool): Deprecated. If True, stores the dataframe in the .json format.
-        Defaults to False.
       drift_evaluator (Callable): Function that evaluates context and return information
         about how drift should be handled. See `drift_evaluator` module.
 
@@ -71,7 +68,6 @@ def store_metric(
         assignees,
         working_branch,
         drift_branch,
-        store_json,
         file_path,
         repo,
         drift_evaluator,
@@ -123,7 +119,6 @@ def partition_and_store_table(
             filepath=monthly_filepath,
             branch=branch,
             assignees=None,
-            store_json=False,
             drift_evaluator=auto_merge_drift,
         )
 
@@ -133,7 +128,6 @@ def push_metric(
     assignees,
     default_branch,
     drift_branch,
-    store_json,
     file_path,
     repo,
     drift_evaluator: Callable[[DriftEvaluatorContext], Dict],
@@ -142,9 +136,7 @@ def push_metric(
     contents = assert_file_exists(repo, file_path, ref=default_branch)
     if contents is None:
         print("Metric not found, creating it on branch: " + default_branch)
-        create_file_on_branch(
-            file_path, repo, default_branch, dataframe, assignees, store_json
-        )
+        create_file_on_branch(file_path, repo, default_branch, dataframe, assignees)
         print("Metric stored")
         pass
     else:
@@ -243,21 +235,12 @@ def create_file_on_branch(
     branch: str,
     dataframe: pd.DataFrame,
     assignees: List[str],
-    store_json: bool,
 ):
     commit_message = "New data: " + file_path
     print("Commit: " + commit_message)
     repo.create_file(
         file_path, commit_message, dataframe.to_csv(index=False, header=True), branch
     )
-    if store_json:
-        json_file_path = file_path.replace(".csv", ".json")
-        repo.create_file(
-            json_file_path,
-            "New data (json): " + file_path,
-            dataframe.to_json(orient="records", lines=True, date_format="iso"),
-            branch,
-        )
 
 
 def create_pullrequest(
