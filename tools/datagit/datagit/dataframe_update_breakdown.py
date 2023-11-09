1a886c8a5b5bac8d6a78264057f75c0b4766f992
Sammy Teillet <sammy.teillet@gmail.com>
2023-11-09 14:06:44 +0100

    refactor: remove comments

--- tools/datagit/datagit/dataframe_update_breakdown.py
+++ tools/datagit/datagit/dataframe_update_breakdown.py
@@ -19,26 +19,22 @@ class DataFrameUpdate(TypedDict):
 def dataframe_update_breakdown(
     initial_dataframe: pd.DataFrame, final_dataframe: pd.DataFrame
 ) -> Dict[str, DataFrameUpdate]:
-    # Ensure the dataframes have the same index
     initial_dataframe = initial_dataframe.set_index(initial_dataframe.columns[0])
     final_dataframe = final_dataframe.set_index(final_dataframe.columns[0])
 
-    # Find columns added and removed
     columns_added = set(final_dataframe.columns) - set(initial_dataframe.columns)
     columns_removed = set(initial_dataframe.columns) - set(final_dataframe.columns)
 
-    # 1. Remove the columns
     step1 = initial_dataframe.drop(columns=columns_removed)
 
-    # 2. Add new data based on date
     new_data = final_dataframe.loc[
         ~final_dataframe["date"].isin(initial_dataframe["date"])
     ]
     step2 = pd.concat([step1, new_data[step1.columns]], axis=0)
 
+    # In this step I want to detect and group some updates
     step3 = final_dataframe.drop(columns=columns_added)
 
-    # 4. Add new columns
     step4 = final_dataframe
 
     return {
