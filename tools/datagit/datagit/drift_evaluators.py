9c575c878e0a2c33108fd2c250d9fffc77f0cd2e
Sammy Teillet <sammy.teillet@gmail.com>
2023-11-14 17:48:07 +0100

    feature: add a drift summary parser

--- tools/datagit/datagit/drift_evaluators.py
+++ tools/datagit/datagit/drift_evaluators.py
@@ -1,3 +1,4 @@
+import json
 import traceback
 from typing import Callable, TypedDict
 from datagit.dataset_helpers import compare_dataframes
@@ -18,13 +19,33 @@ def drift_summary_to_string(drift_summary: DriftSummary) -> str:
         f"Deleted Rows:\n{drift_summary['deleted_rows'].to_string()}\n"
         f"Modified Rows Unique Keys:\n{drift_summary['modified_rows_unique_keys']}\n"
         f"Modified Patterns:\n{drift_summary['modified_patterns'].to_string()}\n"
-        f"Drift Summary Json\n"
+        f"Drift Summary Json Begin\n"
         f"Added Rows JSON:\n{drift_summary['added_rows'].to_json(index=True)}\n"
         f"Deleted Rows JSON:\n{drift_summary['deleted_rows'].to_json(index=True)}\n"
         f"Modified Rows Unique Keys List:\n{drift_summary['modified_rows_unique_keys'].to_list()}\n"
         f"Modified Patterns Json:\n{drift_summary['modified_patterns'].to_json()}\n"
         f"Drift Summary Json End\n"
     )
+
+
+def parse_drift_summary(commit_message: str) -> DriftSummary:
+    # Extracting the drift summary part
+    start_tag = "Drift Summary Json Begin\n"
+    end_tag = "\nDrift Summary Json End"
+    start_index = commit_message.find(start_tag) + len(start_tag)
+    end_index = commit_message.find(end_tag)
+    drift_summary_str = commit_message[start_index:end_index]
+
+    # Parsing each part back into DataFrames and Index
+    parts = drift_summary_str.split("\n")
+    drift_summary = DriftSummary(
+        added_rows=pd.read_json(parts[1]),
+        deleted_rows=pd.read_json(parts[3]),
+        modified_rows_unique_keys=pd.Index(json.loads(parts[5])),
+        modified_patterns=pd.read_json(parts[7]),
+    )
+
+    return drift_summary
 
 
 class DriftEvaluatorContext(TypedDict):
