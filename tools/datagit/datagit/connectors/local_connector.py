426f67df026f2a112056f3bd7c0a883e7ed09e64
Sammy Teillet <sammy.teillet@gmail.com>
2023-11-16 15:58:12 +0100

    refactor: move all methods of local store in a class

--- tools/datagit/datagit/connectors/local_connector.py
+++ tools/datagit/datagit/connectors/local_connector.py
@@ -16,10 +16,6 @@ from ..dataframe.dataframe_update_breakdown import (
 import pandas as pd
 from git import Commit, Repo
 
-home_dir = os.path.expanduser("~")
-datadrift_dir = os.path.join(home_dir, ".datadrift")
-os.makedirs(datadrift_dir, exist_ok=True)
-
 
 def store_table(
     *,
@@ -35,7 +31,9 @@ def store_table(
         table_dataframe
     )
 
-    repo = get_or_init_repo(store_name=store_name)
+    local_connector = LocalConnector(store_name=store_name)
+
+    repo = local_connector.repo
     store_dir = repo.working_dir
     print(f"Storing table {table_name} in db {store_dir}")
     table_file_name = f"{table_name}.csv"
@@ -71,74 +69,79 @@ def store_table(
     pass
 
 
-def get_table(*, store_name="default", metric_name: str) -> pd.DataFrame:
-    store_dir = get_or_init_repo(store_name=store_name).working_dir
-    metric_file_name = f"{metric_name}.csv"
-    return pd.read_csv(os.path.join(store_dir, metric_file_name))
+class LocalConnector:
+    home_dir = os.path.expanduser("~")
+    datadrift_dir = os.path.join(home_dir, ".datadrift")
+    os.makedirs(datadrift_dir, exist_ok=True)
 
+    def __init__(self, store_name="default"):
+        self.store_name = store_name
+        self.repo = self.get_or_init_repo(store_name=self.store_name)
+        self.store_dir = self.repo.working_dir
 
-def get_tables(*, store_name="default"):
-    repo = get_or_init_repo(store_name=store_name)
-    csv_files = [
-        os.path.splitext(f)[0]
-        for f in os.listdir(repo.working_dir)
-        if f.endswith(".csv")
-    ]
-    return csv_files
+    def get_table(self, metric_name: str) -> pd.DataFrame:
+        store_dir = self.store_dir
+        metric_file_name = f"{metric_name}.csv"
+        return pd.read_csv(os.path.join(store_dir, metric_file_name))
 
+    def get_tables(self):
+        repo = self.store_dir
+        csv_files = [
+            os.path.splitext(f)[0]
+            for f in os.listdir(repo.working_dir)
+            if f.endswith(".csv")
+        ]
+        return csv_files
 
-def delete_table(*, store_name="default", metric_name: str):
-    # Getting commit history
-    commit_history = list(
-        get_table_history(store_name=store_name, metric_name=metric_name)
-    )
+    def delete_table(self, metric_name: str):
+        # Getting commit history
+        commit_history = list(self.get_table_history(metric_name=metric_name))
 
-    # If there's no commit, exit
-    if not commit_history:
-        return
+        # If there's no commit, exit
+        if not commit_history:
+            return
 
-    repo = get_or_init_repo(store_name=store_name)
-    active_branch = repo.active_branch
+        repo = self.get_or_init_repo(store_name=self.store_name)
+        active_branch = repo.active_branch
 
-    # Create a new copy of main branch
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    keep_main = f"keep_main_{timestamp}"
-    repo.git.checkout("HEAD", b=keep_main)
+        # Create a new copy of main branch
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        keep_main = f"keep_main_{timestamp}"
+        repo.git.checkout("HEAD", b=keep_main)
 
-    # Create a new temporary branch and checkout
-    tmp_branch = f"tmp_branch_{timestamp}"
-    repo.git.checkout("HEAD", b=tmp_branch)
+        # Create a new temporary branch and checkout
+        tmp_branch = f"tmp_branch_{timestamp}"
+        repo.git.checkout("HEAD", b=tmp_branch)
 
-    for commit in commit_history:
-        print(f"Deleting commit {commit.hexsha}")
-        repo.git.rebase("--onto", commit.hexsha + "^", commit.hexsha, tmp_branch)
+        for commit in commit_history:
+            print(f"Deleting commit {commit.hexsha}")
+            repo.git.rebase("--onto", commit.hexsha + "^", commit.hexsha, tmp_branch)
 
-    repo.git.branch("-D", active_branch)
-    repo.git.checkout("HEAD", b=active_branch)
-    repo.git.branch("-D", tmp_branch)
+        repo.git.branch("-D", active_branch)
+        repo.git.checkout("HEAD", b=active_branch)
+        repo.git.branch("-D", tmp_branch)
 
+    def get_table_history(self, metric_name: str) -> Iterator[Commit]:
+        repo = self.get_or_init_repo(store_name=self.store_name)
+        metric_file_name = f"{metric_name}.csv"
+        commits = repo.iter_commits(paths=metric_file_name)
+        return commits
 
-def get_table_history(*, store_name="default", metric_name: str) -> Iterator[Commit]:
-    repo = get_or_init_repo(store_name=store_name)
-    metric_file_name = f"{metric_name}.csv"
-    commits = repo.iter_commits(paths=metric_file_name)
-    return commits
+    @staticmethod
+    def get_or_init_repo(store_name="default"):
+        store_dir = os.path.join(LocalConnector.datadrift_dir, store_name)
+        os.makedirs(store_dir, exist_ok=True)
 
+        try:
+            repo = Repo(store_dir)
+            return repo
+        except:
+            print(f"The store {store_name} does not exist. Creating it now.")
+            repo = Repo.init(store_dir)
+            repo.index.commit("Init DB")
+            return repo
 
-def get_or_init_repo(*, store_name="default"):
-    store_dir = os.path.join(datadrift_dir, store_name)
-    os.makedirs(store_dir, exist_ok=True)
-
-    try:
-        repo = Repo(store_dir)
-        return repo
-    except:
-        print(f"The store {store_name} does not exist. Creating it now.")
-        repo = Repo.init(store_dir)
-        repo.index.commit("Init DB")
-        return repo
-
-
-def delete_store(*, store_name="default"):
-    store_dir = os.path.join(datadrift_dir, store_name)
-    os.system(f"rm -rf {store_dir}")
+    @staticmethod
+    def delete_store(store_name="default"):
+        store_dir = os.path.join(LocalConnector.datadrift_dir, store_name)
+        os.system(f"rm -rf {store_dir}")
