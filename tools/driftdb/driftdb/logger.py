d37d9711118631200bec4e4c5170f9e8131e85c4
Sammy Teillet <sammy.teillet@gmail.com>
2023-11-20 12:17:05 +0100

    feature: add handler only if it does not exist

--- tools/driftdb/driftdb/logger.py
+++ tools/driftdb/driftdb/logger.py
@@ -6,12 +6,13 @@ def get_logger(name):
 
     logger.setLevel(logging.INFO)
 
-    c_handler = logging.StreamHandler()
-    c_handler.setLevel(logging.INFO)
+    if not logger.handlers:
+        c_handler = logging.StreamHandler()
+        c_handler.setLevel(logging.INFO)
 
-    c_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-    c_handler.setFormatter(c_format)
+        c_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+        c_handler.setFormatter(c_format)
 
-    logger.addHandler(c_handler)
+        logger.addHandler(c_handler)
 
     return logger
