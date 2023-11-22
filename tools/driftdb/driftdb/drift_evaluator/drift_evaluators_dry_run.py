554ef82c37769cf32138b8123f03dc40c1633220
Sammy Teillet <sammy.teillet@gmail.com>
2023-11-22 12:11:06 +0100

    refactor: type return of dry_run (#258)
    * refactor: type return of dry_run
    
    * feature: add dry run for new data evaluator

--- tools/driftdb/driftdb/drift_evaluator/drift_evaluators_dry_run.py
+++ tools/driftdb/driftdb/drift_evaluator/drift_evaluators_dry_run.py
@@ -4,12 +4,15 @@ import pandas as pd
 from github import Github
 
 from ..logger import get_logger
-from .drift_evaluators import BaseDriftEvaluator, DriftEvaluatorContext, parse_drift_summary
+from .drift_evaluators import BaseDriftEvaluator, BaseNewDataEvaluator, DriftEvaluatorContext, parse_drift_summary
+from .interface import DriftEvaluation, NewDataEvaluatorContext
 
 logger = get_logger(__name__)
 
 
-def run_drift_evaluator(*, drift_evaluator: BaseDriftEvaluator, gh_client: Github, repo_name: str, commit_sha: str):
+def run_drift_evaluator(
+    *, drift_evaluator: BaseDriftEvaluator, gh_client: Github, repo_name: str, commit_sha: str
+) -> DriftEvaluation:
     repo = gh_client.get_repo(repo_name)
     commit = repo.get_commit(commit_sha)
     file = commit.files[0]
@@ -48,12 +51,44 @@ def run_drift_evaluator(*, drift_evaluator: BaseDriftEvaluator, gh_client: Githu
         after=new_dataframe,
         summary=drift_summary,
     )
-    try:
-        drift_evaluation = drift_evaluator.compute_drift_evaluation(data_drift_context)
-        #  return result
-        return drift_evaluation
-    except Exception as e:
-        logger.warn("Drift evaluator failed: " + str(e))
-        traceback.print_exc()
-        logger.warn("Drift evaluator failed: " + str(e))
-        traceback.print_exc()
+    drift_evaluation = drift_evaluator.compute_drift_evaluation(data_drift_context)
+    return drift_evaluation
+
+
+def run_new_data_evaluator(
+    *, drift_evaluator: BaseNewDataEvaluator, gh_client: Github, repo_name: str, commit_sha: str
+) -> DriftEvaluation:
+    repo = gh_client.get_repo(repo_name)
+    commit = repo.get_commit(commit_sha)
+    file = commit.files[0]
+    raw_content = repo.get_contents(file.filename, ref=commit_sha)
+    parent_raw_content = repo.get_contents(file.filename, ref=commit.parents[0].sha)
+
+    if isinstance(raw_content, list):
+        raw_content = raw_content[0]
+
+    if isinstance(parent_raw_content, list):
+        parent_raw_content = parent_raw_content[0]
+
+    new_dataframe = pd.read_csv(
+        raw_content.download_url,
+        dtype="string",
+        keep_default_na=False,
+    )
+
+    old_dataframe = pd.read_csv(
+        parent_raw_content.download_url,
+        dtype="string",
+        keep_default_na=False,
+    )
+
+    new_data = new_dataframe.loc[~new_dataframe["unique_key"].isin(old_dataframe["unique_key"])]
+
+    #  run drift evaluator
+    data_drift_context = NewDataEvaluatorContext(
+        before=old_dataframe,
+        after=new_dataframe,
+        added_rows=new_data,
+    )
+    drift_evaluation = drift_evaluator.compute_new_data_evaluation(data_drift_context)
+    return drift_evaluation
