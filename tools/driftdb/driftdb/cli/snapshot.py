0487edc481f6cc54ae2fc589852d0c18e2ff053d
Sammy Teillet <sammy.teillet@gmail.com>
2024-02-02 15:18:24 +0100

    feature: move the datadrift config in a .datadrift.py

--- tools/driftdb/driftdb/cli/snapshot.py
+++ tools/driftdb/driftdb/cli/snapshot.py
@@ -58,7 +58,6 @@ def show(snapshot_id: str = typer.Option(None, help="id of your snapshot")):
 @app.command()
 def check(snapshot_id: str = typer.Option(None, help="id of your snapshot"), date: str = typer.Option(None, help="date of your snapshot")):
     snapshot_node = get_or_prompt_snapshot_node(snapshot_id, get_snapshot_nodes())
-    [handler, transport] = get_user_defined_handlers(snapshot_node)
     snapshot_date = get_user_date_selection(get_snapshot_dates(snapshot_node), date)
 
     if snapshot_date is None:
@@ -67,23 +66,29 @@ def check(snapshot_id: str = typer.Option(None, help="id of your snapshot"), dat
 
     print(f"Getting {snapshot_node['unique_id']} for {snapshot_date}.")
 
-    if not isinstance(transport, AbstractAlertTransport):
-        print("transport is not an instance of AbstractAlertTransport, defaulting to ConsoleAlertTransport.")
-        transport = ConsoleAlertTransport()
+    [drift_handler, alert_transport] = get_user_defined_handlers(snapshot_node)
+
+    if not isinstance(alert_transport, AbstractAlertTransport):
+        print("Alert transport is not an instance of AbstractAlertTransport, defaulting to ConsoleAlertTransport.")
+        alert_transport = ConsoleAlertTransport()
 
 
     diff = get_snapshot_diff(snapshot_node, snapshot_date)
     context = convert_snapshot_to_drift_summary(snapshot_diff=diff, id_column="month", date_column="month")
-    alert = handler(context)
+    alert = drift_handler(context)
     alert_title = f"Drift alert for {snapshot_node['unique_id']} on {snapshot_date}"
-    transport.send(alert_title, alert, context)
+    alert_transport.send(alert_title, alert, context)
 
 
 
 def get_user_defined_handlers(snapshot_node):
     snapshot_file_path = snapshot_node["original_file_path"]
     directory_path = os.path.dirname(snapshot_file_path)
-    user_defined_file_path = os.path.join(directory_path, "datadrift.py")
+    snapshot_file_name = os.path.basename(snapshot_file_path)
+    snapshot_file_name_without_extension, _ = os.path.splitext(snapshot_file_name)
+    user_defined_file_path = os.path.join(directory_path, f"{snapshot_file_name_without_extension}.datadrift.py")
+    
+    print(f"Looking for user defined handlers in {user_defined_file_path}")
 
     try:
         [drift_handler, alert_transport] = import_user_defined_function(user_defined_file_path,[ "drift_handler", "alert_transport"])
